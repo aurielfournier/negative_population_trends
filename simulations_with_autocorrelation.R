1e3b71dd9c9df0442b2471ada7e2c3a68d4c1603
aurielfournier <aurielfournier@gmail.com>
2016-12-09 16:41:08 -0600

    send out to collabs

--- simulations_with_autocorrelation.R
+++ simulations_with_autocorrelation.R
@@ -1,4 +1,4 @@
-
+library(tidyverse)
 
 ################
 #
@@ -9,42 +9,22 @@
 # mu = mean population
 # rho = autocorrelation 
 # CV = Coefficient of Variation
-# K = 2000
+# spp = number of species
 
-twenty_species <- function(nspp=20, nyears=100, mu=1000,rho=0.5,CV=0.2,plot=F, K=2000) {
-  
-  ## SD parameter of lognormal distribution with desired variability
-  ln_SD <- sqrt(log(CV^2+1))  
-  
-  ## SD for first value
-  ln_SD_e <- sqrt(ln_SD^2/(1-rho^2))  
-  
-  ## creates empty objects to store results in
-  R <- matrix(nrow=nyears, ncol=nspp)
-  ## so each row is a year, and each column is a species
-  time_series <- matrix(nrow=nyears, ncol=nspp)
-  
-  ## randomly pulls an R for each species
-  for(r in 1:nspp){
-    R[,r] <- rnorm(mean=0,sd=1,n=nyears)
+generate_species <- function(nyears=100, mu=1000,rho=0.5,CV=0.2, spp=20) {
+  sim.years <- nyears+10
+  ln_SD <- sqrt(log(CV^2+1))  #SD parameter of lognormal distribution with desired variability
+  ln_SD_e <- sqrt(ln_SD^2/(1-rho^2))  #SD for first value
+  time_series_temp <- matrix(rnorm(mean=0,sd=1,n=sim.years*spp),ncol=spp, nrow=sim.years)
+  time_series <- matrix(nrow=sim.years,ncol=spp)
+  time_series[1,] <- ln_SD_e*time_series_temp[1,]
+  for (i in 2:(sim.years)) {
+    time_series[i,] <- rho*time_series[(i-1),]+time_series_temp[i,]*ln_SD
   }
-  
-  
-  for(spp in 1:nspp){
-    ## Sets the initial population size for each species (column)
-    time_series[1,spp] <- ln_SD_e*R[1,spp]
-    for (i in 1:(nyears-1)) {
-      ## Runs the population model for each species (column)
-      time_series[i+1,spp] <- rho*time_series[i,spp]+R[i,spp]*(1-time_series[i,spp]/K)*ln_SD
-    }
-  }
-  
   time_series <- time_series + log(mu) - 0.5*ln_SD_e^2
   time_series <- exp(time_series)
-  
-  return(time_series)
+  return(time_series[-c(1:10),])
 }
-
 
 ################
 #
@@ -53,7 +33,7 @@ twenty_species <- function(nspp=20, nyears=100, mu=1000,rho=0.5,CV=0.2,plot=F, K
 ################
 
 
-realtrend <- function(time_series, nspeciestopick=2){
+realtrend <- function(time_series){
   
   ## finds the two species with the highest populations in year 1 
   one <- time_series[,which(time_series[1,]==sort(time_series[1,], TRUE)[1])]
@@ -61,14 +41,14 @@ realtrend <- function(time_series, nspeciestopick=2){
   year = 1:nrow(time_series)
   
   ## puts the three vectors into one data frame, ready to be sampled from  
-  twospecies <-  data.frame(one, two, year)
+  highestspecies <-  data.frame(one, two, year)
   
   ## Creates empty object to put results into
   values <- data.frame(beta=c(NA,NA),pvalue=c(NA,NA), rsquared=c(NA,NA))
   
   ## run two models, one on the first species, one on the second
-  speciesmodel1 <- lm(data=twospecies, one ~ year)
-  speciesmodel2 <- lm(data=twospecies, two ~ year)
+  speciesmodel1 <- lm(data=highestspecies, one ~ year)
+  speciesmodel2 <- lm(data=highestspecies, two ~ year)
   
   ## extract the beta coefficient, pvalue and rsquared for the first species
   values[1,1] <- speciesmodel1$coefficients[2]
@@ -82,8 +62,38 @@ realtrend <- function(time_series, nspeciestopick=2){
   
   ## puts everything in a list to be output
   realstuff <- list()
-  realstuff[["values"]] <- values
-  realstuff[["twospecies"]] <- twospecies
+  realstuff[["real_values_highest"]] <- values
+  realstuff[["real_data_highest"]] <- highestspecies
+  
+  ###########
+  
+  one <- time_series[,base::sample(1:ncol(time_series),1)]
+  two <- time_series[,base::sample(1:ncol(time_series),1)]
+  year = 1:nrow(time_series)
+  
+  ## puts the three vectors into one data frame, ready to be sampled from  
+  randomspecies <-  data.frame(one, two, year)
+  
+  ## Creates empty object to put results into
+  values <- data.frame(beta=c(NA,NA),pvalue=c(NA,NA), rsquared=c(NA,NA))
+  
+  ## run two models, one on the first species, one on the second
+  speciesmodel1 <- lm(data=randomspecies, one ~ year)
+  speciesmodel2 <- lm(data=randomspecies, two ~ year)
+  
+  ## extract the beta coefficient, pvalue and rsquared for the first species
+  values[1,1] <- speciesmodel1$coefficients[2]
+  values[1,2] <- summary(speciesmodel1)$coefficients[,4][2]
+  values[1,3] <- summary(speciesmodel1)$r.squared
+  
+  ## extract the beta coefficient, pvalue and rsquared for the second species
+  values[2,1] <- speciesmodel2$coefficients[2]
+  values[2,2] <- summary(speciesmodel2)$coefficients[,4][2]
+  values[2,3] <- summary(speciesmodel2)$r.squared
+  
+  ## puts everything in a list to be output
+  realstuff[["real_values_random"]] <- values
+  realstuff[["real_data_random"]] <- randomspecies
   
   return(realstuff)
 }
@@ -125,9 +135,48 @@ samplingeffortwitherror <- function(time_series, freq=NA, sd=0.1){
   
   ## This takes the years we just sampled, and applies an error term
   ## since its pretty hard to actually estimate the population
-  sampled_data[,1:2] <- sampled_data[,1:2] * exp(rnorm(1, mean=0, sd=sd)-0.5*sd^2)
+  sdata <- list()
+  sampled_data[,1:2] <-  sampled_data[,1:2] * exp(rnorm(1, mean=0, sd=sd)-0.5*sd^2)
+  sdata[["sampled_data_highest"]] <- sampled_data
   
-  return(sampled_data)
+  ####
+  #
+  # two randomly selected
+  #
+  ####
+  
+  ## The default is to have freq=NA, which means that the function 
+  ## will randomly pull a random set of years to sample on
+  ## This checks to see if freq=NA, and if so, does that
+  ## If we set freq = to some number it will bipass that and use that instead
+  if(is.na(freq)){
+    freq <- sort(base::sample(1:100, sample(1:60, 1), replace=FALSE))
+  }
+  
+  ## grabs two species at random
+  one <- time_series[,base::sample(1:ncol(time_series),1)]
+  two <- time_series[,base::sample(1:ncol(time_series),1)]
+  year = 1:nrow(time_series)
+  
+  ## puts the three vectors into one data frame, ready to be sampled from  
+  twospecies <-  data.frame(one, two, year)
+  
+  ## If we have set freq = to some single value, this if statement runs
+  if(length(freq)==1){
+    sampled_data <- twospecies[seq(from=1, to=nrow(time_series), by=freq),]
+  }
+  
+  ## if freq is a vector of years, than this one runs
+  if(length(freq)>1){
+    sampled_data <- twospecies[freq,]
+  }
+  
+  ## This takes the years we just sampled, and applies an error term
+  ## since its pretty hard to actually estimate the population
+  sampled_data[,1:2] <-  sampled_data[,1:2] * exp(rnorm(1, mean=0, sd=sd)-0.5*sd^2)
+  sdata[["sampled_data_random"]] <- sampled_data
+  
+  return(sdata)
 }
 
 ##################
@@ -137,13 +186,17 @@ samplingeffortwitherror <- function(time_series, freq=NA, sd=0.1){
 ##################
 
 sampletrend <- function(sampled_data){
-  
+
+  ##
+  # Two Highest
+  ##
+    
   ## Create empty object to store results in
   sampled_values <- data.frame(beta=c(NA,NA),pvalue=c(NA,NA),rsquared=c(NA,NA))
   
   ## run two models, one on the first species, one on the second
-  speciesmodel1 <- lm(data=sampled_data, one ~ year)
-  speciesmodel2 <- lm(data=sampled_data, two ~ year)
+  speciesmodel1 <- lm(data=sampled_data$sampled_data_highest, one ~ year)
+  speciesmodel2 <- lm(data=sampled_data$sampled_data_highest, two ~ year)
   
   ## extract the beta coefficient, pvalue and rsquared for the first species
   sampled_values[1,1] <- speciesmodel1$coefficients[2]
@@ -155,7 +208,32 @@ sampletrend <- function(sampled_data){
   sampled_values[2,2] <- summary(speciesmodel2)$coefficients[,4][2]
   sampled_values[2,3] <- summary(speciesmodel2)$r.squared
   
-  return(sampled_values)
+  sampled_trend <- list()
+  sampled_trend[["sampled_trend_highest"]] <- sampled_values
+  ##
+  # Two Random
+  ## 
+  
+  ## Create empty object to store results in
+  sampled_values <- data.frame(beta=c(NA,NA),pvalue=c(NA,NA),rsquared=c(NA,NA))
+  
+  ## run two models, one on the first species, one on the second
+  speciesmodel1 <- lm(data=sampled_data$sampled_data_random, one ~ year)
+  speciesmodel2 <- lm(data=sampled_data$sampled_data_random, two ~ year)
+  
+  ## extract the beta coefficient, pvalue and rsquared for the first species
+  sampled_values[1,1] <- speciesmodel1$coefficients[2]
+  sampled_values[1,2] <- summary(speciesmodel1)$coefficients[,4][2]
+  sampled_values[1,3] <- summary(speciesmodel1)$r.squared
+  
+  ## extract the beta coefficient, pvalue and rsquared for the second species  
+  sampled_values[2,1] <- speciesmodel2$coefficients[2]
+  sampled_values[2,2] <- summary(speciesmodel2)$coefficients[,4][2]
+  sampled_values[2,3] <- summary(speciesmodel2)$r.squared
+  
+  sampled_trend[["sampled_trend_random"]] <- sampled_values
+  
+  return(sampled_trend)
 }
 
 ##################
@@ -167,20 +245,28 @@ sampletrend <- function(sampled_data){
 onesimulation <- function(freq=NA){
   
   ## Generate the time series for 20 species
-  time_series <- twenty_species()
+  time_series <- generate_species()
+  
   ## Figure out the 'real trend'
   rdat <- realtrend(time_series)
+  
   ## Sample the real population data, with some error
   sampled_data <- samplingeffortwitherror(time_series, freq=freq, sd=0.1)
+  
   ## figure out what the trend is for the sampled_data
   svalues <- sampletrend(sampled_data)
+  
   ## output everything into one master list
   outputs <- list()
   outputs[["twenty_species"]] <- time_series
-  outputs[["values"]] <- rdat$values
-  outputs[["twospecies"]] <- rdat$twospecies
-  outputs[["sampled_data"]] <- sampled_data
-  outputs[["sampled_values"]] <- svalues
+  outputs[["real_values_highest"]] <- rdat$real_values_highest
+  outputs[["real_values_random"]] <- rdat$real_values_random
+  outputs[["real_data_highest"]] <- rdat$real_data_highest
+  outputs[["real_data_random"]] <- rdat$real_data_random
+  outputs[["sampled_data_highest"]] <- sampled_data$sampled_data_highest
+  outputs[["sampled_data_random"]] <- sampled_data$sampled_data_random
+  outputs[["sampled_values_highest"]] <- svalues$sampled_trend_highest
+  outputs[["sampled_values_random"]] <- svalues$sampled_trend_random
   return(outputs)
 }
 
@@ -193,30 +279,50 @@ onesimulation <- function(freq=NA){
 manysimulations <- function(sims=1000, freq=NA){
   
   ## Create empty objects to store results in
-  values <- data.frame(beta=rep(NA,sims*2),pvalue=rep(NA,sims*2),rsquared=rep(NA,sims*2))
-  svalues <- data.frame(beta=rep(NA,sims*2),pvalue=rep(NA,sims*2),rsquared=rep(NA,sims*2))
-  twospecies <- as.data.frame(matrix(ncol=(2*sims), nrow=100))
-  sampleddata <- list()
+  ## sampledata is a list because the number of rows could vary from sim to sim
+  real_values_highest <- data.frame(beta=rep(NA,sims*2),pvalue=rep(NA,sims*2),rsquared=rep(NA,sims*2))
+  real_values_random <- data.frame(beta=rep(NA,sims*2),pvalue=rep(NA,sims*2),rsquared=rep(NA,sims*2))
+  sampled_values_highest <- data.frame(beta=rep(NA,sims*2),pvalue=rep(NA,sims*2),rsquared=rep(NA,sims*2))
+  sampled_values_random <- data.frame(beta=rep(NA,sims*2),pvalue=rep(NA,sims*2),rsquared=rep(NA,sims*2))
+  real_data_highest <- as.data.frame(matrix(ncol=(2*sims), nrow=100))
+  real_data_random <- as.data.frame(matrix(ncol=(2*sims), nrow=100))
+  sampled_data_highest <- list()
+  sampled_data_random <- list()
   
   ## Runs through the onesimulation function sims number of times
   ## stores the various results in the different objects
-  ## sampledata is a list because the number of rows could vary from sim to sim
   for(i in seq(1,(sims*2),by=2)){
     dd <- onesimulation(freq=freq)
-    values[i:(i+1),] <- dd$values
-    svalues[i:(i+1),] <- dd$sampled_values
-    twospecies[,i:(i+1)] <- dd$twospecies[,1:2]
-    sampleddata[[i]] <- dd$sampled_data[,1:2]
+    real_values_highest[i:(i+1),] <- dd$real_values_highest
+    real_values_random[i:(i+1),] <- dd$real_values_random
+    sampled_values_highest[i:(i+1),] <- dd$sampled_values_highest
+    sampled_values_random[i:(i+1),] <- dd$sampled_values_random
+    real_data_highest[,i:(i+1)] <- dd$real_data_highest[,1:2]
+    real_data_random[,i:(i+1)] <- dd$real_data_random[,1:2]
+    sampled_data_highest[[i]] <- dd$sampled_data_highest[,1:2]
+    sampled_data_random[[i]] <- dd$sampled_data_random[,1:2]
   }
   
   ## load everything up into one master list to be outputted
   outputs <- list()
-  outputs[["values"]] <- values
-  outputs[["svalues"]] <- svalues
-  outputs[["twospecies"]] <- twospecies
-  outputs[["sampleddata"]] <- sampleddata
+  outputs[["real_values_highest"]] <- real_values_highest
+  outputs[["real_values_random"]] <- real_values_random
+  outputs[["sampled_values_highest"]] <- sampled_values_highest
+  outputs[["sampled_values_random"]] <- sampled_values_random
+  outputs[["real_data_highest"]] <- real_data_highest
+  outputs[["real_data_random"]] <- real_data_random
+  outputs[["sampled_data_highest"]] <- sampled_data_highest 
+  outputs[["sampled_data_random"]] <- sampled_data_random
   
   return(outputs)
+  
+  # if you get this warning
+  
+  # In if (is.na(freq)) { :
+  #the condition has length > 1 and only the first element will be used
+  
+  # DO NOT FREAK OUT, EVERYTHING IS FINE
+  # It just means you fed it a string of years to subset by and it is only checking to see if the first one is an NA. Which is ok. 
   
 }
 
@@ -226,23 +332,41 @@ manysimulations <- function(sims=1000, freq=NA){
 
 ###########################################################
 
-dat <- manysimulations()
+dat <- manysimulations(sim=10)
 
-library(tidyverse)
 
-values <- dat$values
-svalues <- dat$svalues
 
-valuessig <- values %>% filter(pvalue<=0.05) %>% mutate(type="realtrend")
-svaluessig <- svalues %>% filter(pvalue<=0.05) %>% mutate(type="surveyeddata")
+values <- dat$real_values_highest
+svalues <- dat$sampled_values_highest
 
-datdat <- bind_rows(valuessig, svaluessig)
+valuessig <- values %>% filter(pvalue<=0.05) %>% mutate(type="realtrend_significant")
+svaluessig <- svalues %>% filter(pvalue<=0.05) %>% mutate(type="surveyeddata_significant")
 
-model <- aov(data=datdat, beta ~ type)
+sigdat <- bind_rows(valuessig, svaluessig)
+
+model <- aov(data=sigdat, beta ~ type)
 
 summary(model)
 
-ggplot(data=datdat, aes(y=beta, x=type))+ geom_boxplot()
+ggplot(data=sigdat, aes(y=beta, x=type))+ geom_boxplot() + ylim(-10,10)
+
+#
+#
+#
+
+values <- dat$real_values_random
+svalues <- dat$sampled_values_random
+
+valuessig <- values %>% filter(pvalue<=0.05) %>% mutate(type="realtrend_significant")
+svaluessig <- svalues %>% filter(pvalue<=0.05) %>% mutate(type="surveyeddata_significant")
+
+sigdat <- bind_rows(valuessig, svaluessig)
+
+model <- aov(data=sigdat, beta ~ type)
+
+summary(model)
+
+ggplot(data=sigdat, aes(y=beta, x=type))+ geom_boxplot() + ylim(-10,10)
 
 
 ###############
@@ -253,22 +377,38 @@ ggplot(data=datdat, aes(y=beta, x=type))+ geom_boxplot()
 
 dat <- manysimulations(freq=4)
 
-library(tidyverse)
 
-values <- dat$values
-svalues <- dat$svalues
+values <- dat$real_values_highest
+svalues <- dat$real_values_highest
 
-valuessig <- values %>% filter(pvalue<=0.05) %>% mutate(type="realtrend")
-svaluessig <- svalues %>% filter(pvalue<=0.05) %>% mutate(type="surveyeddata")
+valuessig <- values %>% filter(pvalue<=0.05) %>% mutate(type="realtrend_significant")
+svaluessig <- svalues %>% filter(pvalue<=0.05) %>% mutate(type="surveyeddata_significant")
 
-datdat <- bind_rows(valuessig, svaluessig)
+sigdat <- bind_rows(valuessig, svaluessig)
 
-model <- aov(data=datdat, beta ~ type)
+model <- aov(data=sigdat, beta ~ type)
 
 summary(model)
 
-ggplot(data=datdat, aes(y=beta, x=type))+ geom_boxplot()
+ggplot(data=sigdat, aes(y=beta, x=type))+ geom_boxplot() + ylim(-10,10)
 
+#
+#
+#
+
+values <- dat$real_values_random
+svalues <- dat$real_values_random
+
+valuessig <- values %>% filter(pvalue<=0.05) %>% mutate(type="realtrend_significant")
+svaluessig <- svalues %>% filter(pvalue<=0.05) %>% mutate(type="surveyeddata_significant")
+
+sigdat <- bind_rows(valuessig, svaluessig)
+
+model <- aov(data=sigdat, beta ~ type)
+
+summary(model)
+
+ggplot(data=sigdat, aes(y=beta, x=type))+ geom_boxplot() + ylim(-10,10)
 
 ##################
 
@@ -278,22 +418,40 @@ ggplot(data=datdat, aes(y=beta, x=type))+ geom_boxplot()
 
 bursts_of_sampling <- c(1,2,3,4,5,6,15,16,18,19,37,38,39,47,48,56,69,70,71,72,73,74,75,76,77,78,91,92,93,97,99)
 
-dat <- manysimulations(freq=bursts_of_sampling)
+dat <- manysimulations(freq=bursts_of_sampling, sims=10000)
 
-library(tidyverse)
 
-values <- dat$values
-svalues <- dat$svalues
+values <- dat$real_values_highest
+svalues <- dat$real_values_highest
 
-valuessig <- values %>% filter(pvalue<=0.05) %>% mutate(type="realtrend")
-svaluessig <- svalues %>% filter(pvalue<=0.05) %>% mutate(type="surveyeddata")
+valuessig <- values %>% filter(pvalue<=0.05) %>% mutate(type="realtrend_significant")
+svaluessig <- svalues %>% filter(pvalue<=0.05) %>% mutate(type="surveyeddata_significant")
 
-datdat <- bind_rows(valuessig, svaluessig)
+sigdat <- bind_rows(valuessig, svaluessig)
 
-model <- aov(data=datdat, beta ~ type)
+model <- aov(data=sigdat, beta ~ type)
 
 summary(model)
 
-ggplot(data=datdat, aes(y=beta, x=type))+ geom_boxplot()
+ggplot(data=sigdat, aes(y=beta, x=type))+ geom_boxplot() + ylim(-10,10)
+
+#
+#
+#
+
+values <- dat$real_values_random
+svalues <- dat$real_values_random
+
+valuessig <- values %>% filter(pvalue<=0.05) %>% mutate(type="realtrend_significant")
+svaluessig <- svalues %>% filter(pvalue<=0.05) %>% mutate(type="surveyeddata_significant")
+
+sigdat <- bind_rows(valuessig, svaluessig)
+
+model <- aov(data=sigdat, beta ~ type)
+
+summary(model)
+
+ggplot(data=sigdat, aes(y=beta, x=type))+ geom_boxplot() + ylim(-10,10)
+
 
 
